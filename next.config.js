37f22e0d8febb59d76442a572743c85dbf7532dd
JOON HYUK LEE <anshqhsh@naver.com>
2022-12-02 00:00:41 +0900

    scss setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: true, // 필요없는 공백이나, 주석을 삭제하여 용량을 줄이고, 해당 스크립트를 해석할 수 없도록 암호화 하는 역할
+  sassOptions: {
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+      prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
